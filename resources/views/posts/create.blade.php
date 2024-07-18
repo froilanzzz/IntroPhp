bc8492e12e05e0b448eb239ee7fc4dcc2a350100
Marc Froilan Abaoag <110769075+Froilanz23@users.noreply.github.com>
2024-07-18 03:52:07 -1100

    first commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,15 @@
+<form action="">
+<div>
+    <p>Title:</p>
+    <input type="text" name="title" id="title">
+</div>
+
+<div>
+    <p>Content:</p>
+    <input type="text" name="content" id="content">
+</div>
+
+<br>
+<button>Save</button>
+
+</form>
